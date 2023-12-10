433cf0af1988bba8c456f74a3222476bc2cd7864
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-10 08:41:55 -0300

    Create method Edit/Delete and config OCORS

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,8 +1,12 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +14,7 @@ import br.com.projeto.api.model.Customer;
 import br.com.projeto.api.repository.CustomerRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controller {
 
     @Autowired
@@ -23,6 +28,16 @@ public class Controller {
     @GetMapping("/")
     public Iterable<Customer> select(){
         return action.findAll();
+    }
+    
+    @PutMapping
+    public Customer edit(@RequestBody Customer customer){
+        return action.save(customer);
+    }
+
+    @DeleteMapping("/{code}")
+    public void remove(@PathVariable long code){
+        action.deleteById(code);
     }
 
 }
