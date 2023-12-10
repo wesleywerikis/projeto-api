f43399e4e580f0efcd17f5681f3db66227a5f96e
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-10 07:57:35 -0300

    Created the customer registration method and the list

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,14 +1,28 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.model.Customer;
+import br.com.projeto.api.repository.CustomerRepository;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/hello")
-    public String teste(){
-        return "Hello World";
+    @Autowired
+    private CustomerRepository action;
+
+    @PostMapping("/")
+    public Customer register(@RequestBody Customer customer){
+        return action.save(customer);
+    }
+
+    @GetMapping("/")
+    public Iterable<Customer> select(){
+        return action.findAll();
     }
 
 }
