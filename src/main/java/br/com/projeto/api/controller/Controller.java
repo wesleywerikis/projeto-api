153cffe3ff62c7c7e86ed4f342be5cbeef15eed4
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-10 07:17:21 -0300

    Create test to run the application

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,5 +1,14 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
-    
+
+    @GetMapping("/hello")
+    public String teste(){
+        return "Hello World";
+    }
+
 }
