bd3edf993e32e8a7016daf59c719a43be0214f1d
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-10 07:24:08 -0300

    Added attributes to my model

--- src/main/java/br/com/projeto/api/model/Customer.java
+++ src/main/java/br/com/projeto/api/model/Customer.java
@@ -1,5 +1,25 @@
 package br.com.projeto.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "costumers")
+@Getter
+@Setter
 public class Customer {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long code;
+    private String name;
+    private int age;
+    private String city;
+
+
 }
